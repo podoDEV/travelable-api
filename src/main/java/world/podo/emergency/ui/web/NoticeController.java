e933a4944a9cd6085a99e5b7355c37f7343b1240
전해성 <seong0428@gmail.com>
2020-05-04 14:32:10 +0900

    refactor - Fix typo

--- src/main/java/world/podo/emergency/ui/web/NoticeController.java
+++ src/main/java/world/podo/emergency/ui/web/NoticeController.java
@@ -1,11 +1,11 @@
 package world.podo.emergency.ui.web;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
 import java.util.Collections;
 
 @RestController
