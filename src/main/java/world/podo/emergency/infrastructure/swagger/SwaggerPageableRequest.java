2f58299c296a5fe7456e1e3207f1b8d139c6dacb
전해성 <seong0428@gmail.com>
2020-05-04 14:03:06 +0900

    feat #15 - swagger 에서 pageable 예쁘게 보여주기

---
+++ src/main/java/world/podo/emergency/infrastructure/swagger/SwaggerPageableRequest.java
@@ -0,0 +1,25 @@
+package world.podo.emergency.infrastructure.swagger;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+class SwaggerPageableRequest {
+    @ApiModelProperty(
+            value = "Number of records per page",
+            example = "20"
+    )
+    private Integer size;
+
+    @ApiModelProperty(
+            value = "Results page you want to retrieve (0..N)",
+            example = "0"
+    )
+    private Integer page;
+
+    @ApiModelProperty(
+            value = "Sorting criteria in the format: property(,asc|desc). Default sort order is ascending. Multiple sort criteria are supported.",
+            example = "&sort=created,asc"
+    )
+    private String sort;
+}
