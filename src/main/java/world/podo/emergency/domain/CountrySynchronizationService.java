543bfbd4deaa8066cd18af5a192f56a32cc54558
전해성 <seong0428@gmail.com>
2020-05-04 14:07:43 +0900

    refactor - Remove an unnecessary file

--- src/main/java/world/podo/emergency/domain/CountrySynchronizationService.java
+++ src/main/java/world/podo/emergency/domain/CountrySynchronizationService.java
@@ -1,5 +1,23 @@
 package world.podo.emergency.domain;
 
-public interface CountrySynchronizationService {
-    Country synchronize(CountryFetchValue countryFetchValue);
+import lombok.RequiredArgsConstructor;
+
+@DomainService
+@RequiredArgsConstructor
+public class CountrySynchronizationService {
+    private final CountryService countryService;
+    private final CountryFactory countryFactory;
+
+    public Country synchronize(CountryFetchValue countryFetchValue) {
+        return countryService.getCountryByCoutryProviderId(countryFetchValue.getId())
+                             .map(country -> country.update(
+                                     countryFetchValue.getId(),
+                                     countryFetchValue.getName(),
+                                     countryFetchValue.getEnglishName(),
+                                     countryFetchValue.getContinent(),
+                                     countryFetchValue.getBasic(),
+                                     countryFetchValue.getImageUrl()
+                             ))
+                             .orElseGet(() -> countryFactory.create(countryFetchValue));
+    }
 }
