8b5095ac5be42711247953ae186003e29b9f0371
전해성 <seong0428@gmail.com>
2020-06-06 16:02:00 +0900

    feat - 샘플 응답을 리스트로 변경

--- src/main/java/world/podo/emergency/ui/web/SampleController.java
+++ src/main/java/world/podo/emergency/ui/web/SampleController.java
@@ -4,11 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class SampleController {
     @GetMapping("sample")
-    public ApiResponse<SampleResponse> getSamples() {
-        return ApiResponse.data(SampleResponse.ghana());
+    public ApiResponse<List<SampleResponse>> getSamples() {
+        return ApiResponse.data(
+                Collections.singletonList(
+                        SampleResponse.ghana()
+                )
+        );
     }
 }
