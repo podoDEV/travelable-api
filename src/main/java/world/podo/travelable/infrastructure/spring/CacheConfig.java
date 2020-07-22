61b56ea3d80c519fe446ae8476d0634b3144417b
Haeseong Jeon <seong0428@gmail.com>
2020-07-22 22:46:19 +0900

    feat: 캐시 TTL 추가

--- src/main/java/world/podo/travelable/infrastructure/spring/CacheConfig.java
+++ src/main/java/world/podo/travelable/infrastructure/spring/CacheConfig.java
@@ -1,12 +1,16 @@
 package world.podo.travelable.infrastructure.spring;
 
+import com.google.common.cache.CacheBuilder;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
@@ -14,7 +18,18 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager(CacheName.names());
+        return new ConcurrentMapCacheManager(CacheName.names()) {
+            @Override
+            protected Cache createConcurrentMapCache(String name) {
+                return new ConcurrentMapCache(
+                        name,
+                        CacheBuilder.newBuilder()
+                                    .expireAfterWrite(2L, TimeUnit.DAYS)
+                                    .build()
+                                    .asMap(),
+                        false);
+            }
+        };
     }
 
     public enum CacheName {
