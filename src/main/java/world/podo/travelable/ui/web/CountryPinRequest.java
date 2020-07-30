9e0980c968d1d152125c13b5e76ea67a45ce9fea
전해성 <seong0428@gmail.com>
2020-07-30 21:19:39 +0900

    feat: LocalDateTime MAX, MIN 값 수정

--- src/main/java/world/podo/travelable/ui/web/CountryPinRequest.java
+++ src/main/java/world/podo/travelable/ui/web/CountryPinRequest.java
@@ -2,11 +2,13 @@ package world.podo.travelable.ui.web;
 
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Data
 public class CountryPinRequest {
-    private LocalDateTime beginAt = LocalDateTime.MIN;
-    private LocalDateTime endAt = LocalDateTime.MAX;
+    private LocalDateTime beginAt = LocalDateTime.of(LocalDate.of(1970, 1, 1), LocalTime.MIN);
+    private LocalDateTime endAt = LocalDateTime.of(LocalDate.of(2100, 12, 31), LocalTime.MAX);
     private Boolean alarmEnabled = Boolean.TRUE;
 }
